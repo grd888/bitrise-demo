4fcd7500431354f76183dfc41f457dab8408663a
Greg Delgado <grd888@gmail.com>
2021-10-25 18:01:03 +0800

    Initial Commit

---
+++ bitrise demo/bitrise_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitrise_demoApp.swift
+//  bitrise demo
+//
+//  Created by GD on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct bitrise_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
